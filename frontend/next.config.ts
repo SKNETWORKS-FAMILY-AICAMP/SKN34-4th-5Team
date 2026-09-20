e9c4c25b0a83b3fd08c063035c9396b015c19f87
이현준 <skagksrkd22@gmail.com>
2026-09-20 23:33:03 +0900

    chore: 테스트

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "localhost", "ec2-3-27-40-249.ap-southeast-2.compute.amazonaws.com"],
+  allowedDevOrigins: ["127.0.0.1", "localhost", "ec2-13-125-74-210.ap-northeast-2.compute.amazonaws.com"],
   images: {
     remotePatterns: [{ protocol: "https", hostname: "image.tving.com", pathname: "/ntgs/sports/kbo/**" }],
   },
