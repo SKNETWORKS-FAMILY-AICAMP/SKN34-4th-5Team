dacd0670bec0fccb9d3bf2b4c9c44dd4238e543f
이현준 <skagksrkd22@gmail.com>
2026-09-21 23:39:19 +0900

    chore: 도커 설정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
 
+const allowedDevOrigins = (
+  process.env.NEXT_ALLOWED_DEV_ORIGINS || "127.0.0.1,localhost"
+)
+  .split(",")
+  .map((origin) => origin.trim());
+
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "localhost", "ec2-13-125-74-210.ap-northeast-2.compute.amazonaws.com"],
+  allowedDevOrigins,
+
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "image.tving.com", pathname: "/ntgs/sports/kbo/**" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tving.com",
+        pathname: "/ntgs/sports/kbo/**",
+      },
+    ],
   },
 };
 
